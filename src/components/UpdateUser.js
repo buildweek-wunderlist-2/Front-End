784126bf3891e7b144ac46750c5afd7cf68efcac
Daniel Hoesle <dhoesle21@gmail.com>
2020-08-25 22:23:54 -0600

    added user edit function

--- src/components/UpdateUser.js
+++ src/components/UpdateUser.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'
 import uuid from 'react-uuid'
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
 
 const initialUser = {
@@ -7,25 +10,54 @@ const initialUser = {
     username: '',
     email: '',
     password: '',
-    time: '',
 };
 
-const UpdateUser = () => {
+const UpdateUser = props => {
+    const location = useLocation()
     const [user, setUser] = useState(initialUser)
+    const { push } = useHistory();
 
+    console.log("props", props)
+    useEffect(() => {
+        if (location.state) {
+            setUser(location.state)
+        } else {
+            setUser(props.user)
+        }
+    }, [])
 
+    const changeHandler = evt => {
+        evt.persist();
+        let value = evt.target.value
+        setUser({
+            ...user,
+            [evt.target.name]: value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axiosWithAuth()
+            .put(`/api/users/${props.user.id}`, user)
+            .then(res => {
+                console.log(res)
+                push(`/protected`)
+                window.location.reload()
+                
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
         <div>
             <h2>Update User</h2>
-            <from>
+            <form onSubmit={handleSubmit}>
                 <label>Username</label>
                 <input
                 type="text"
                 name="username"
-                // onChange={}
-                placeholder="username"
-                // value={}
+                onChange={changeHandler}
+                value={user.username}
                 />
                 <div className="baseline" />
                 <label>email</label>
@@ -33,9 +65,9 @@ const UpdateUser = () => {
                 <input
                 type="string"
                 name="email"
-                // onChange={}
+                onChange={changeHandler}
                 placeholder="email"
-                // value={}
+                value={user.email}
                 />
                 <div className="baseline" />
                 <label>password</label>
@@ -43,15 +75,15 @@ const UpdateUser = () => {
                 <input
                 type="password"
                 name="password"
-                // onChange={}
+                onChange={changeHandler}
                 placeholder="password"
-                // value={}
+                value={user.password}
                 />
                 <div className="baseline" />
                 
                 <button className="md-button form-button">Update</button>
 
-            </from>
+            </form>
         </div>
     )
 };
